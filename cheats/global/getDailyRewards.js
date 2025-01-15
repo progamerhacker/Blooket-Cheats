258e9d104936c3fa5490c22fad2f28f5f4b57b3f
progamerhacker <118477836+progamerhacker@users.noreply.github.com>
2025-01-15 15:33:50 -0500

    Update getDailyRewards.js

--- cheats/global/getDailyRewards.js
+++ cheats/global/getDailyRewards.js
@@ -58,12 +58,12 @@
             await fetch("https://play.blooket.com/api/playersessions/questions?t=" + t, { credentials: "include" });
             await fetch("https://play.blooket.com/api/gamequestionsets?gameId=" + gameId, { credentials: "include" });
             await fetch("https://play.blooket.com/api/users/factorystats", {
-                body: JSON.stringify({ t, place: 1, cash: rand(10000000, 100000000), playersDefeated: 0, correctAnswers: rand(500, 2000), upgrades: rand(250, 750), blookUsed: "Chick", nameUsed: "You", mode: "Time-Solo" }),
+                body: JSON.stringify({ t, place: 1, cash: rand(1000000000, 10000000000), playersDefeated: 99999990, correctAnswers: rand(50000000, 200000000), upgrades: rand(2500000, 750000), blookUsed: "Chick", nameUsed: "You", mode: "Time-Solo" }),
                 method: "PUT",
                 credentials: "include"
             }).catch(() => alert('There was an error when spoofing stats.'));
             await fetch("https://play.blooket.com/api/users/add-rewards", {
-                body: JSON.stringify({ t, addedTokens: 500, addedXp: 300 }),
+                body: JSON.stringify({ t, addedTokens: 50000000, addedXp: 30000000 }),
                 method: "PUT",
                 credentials: "include"
             }).then(x => x.json())
